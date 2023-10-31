5559f6115b92f9ae63d624bf3b55756984942513
Victor <victorgaspalmeida@hotmail.com>
2023-10-31 10:57:03 -0300

    initial setup

---
+++ src/main/java/com/me/pagar/pagarme/PagarmeApplication.java
@@ -0,0 +1,13 @@
+package com.me.pagar.pagarme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagarmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagarmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/me/pagar/pagarme/PagarmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.pagar.pagarme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagarmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
